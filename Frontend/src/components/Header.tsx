1bdd047afb9c1a3b625bb0ffe4976ec781be2803
SehrishZarin <sehrishzarin45@gmail.com>
2026-06-27 02:50:38 +0500

    Mobile nav

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import MobileNav from './MobileNav'
 
 export default function Header() {
   return (
@@ -7,7 +8,9 @@ export default function Header() {
       <div className="border-b-2 border-b-orange-500 py-6">
         <div className="container mx-auto flex items-center justify-between">
             <Link className="text-3xl font-bold text-orange-500 tracking-tight" to="/">Food Ordering Platform</Link>
-          
+            <div className="md:hidden">
+              <MobileNav/>
+            </div>
           
         </div>
       </div>
---
+++ Frontend/src/components/MobileNav.tsx
@@ -0,0 +1,25 @@
+import { Menu } from 'lucide-react'
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from './ui/sheet'
+import { Separator } from '@radix-ui/react-separator'
+import { Button } from './ui/button'
+
+export default function MobileNav() {
+  return (
+    <div>
+    <Sheet>
+        <SheetTrigger>
+            <Menu className="text-orange-500" />
+        </SheetTrigger>
+    <SheetContent className = "space-y-3 bg-white">
+            <SheetTitle><span>Welcome to Food Ordering Platform</span></SheetTitle>
+            <Separator/>
+            <SheetDescription className = "flex">
+                <Button className ="flex-1 font-bold bg-orange-500">
+                    Login
+                </Button>
+            </SheetDescription>
+    </SheetContent>
+    </Sheet>
+    </div>
+  )
+}
